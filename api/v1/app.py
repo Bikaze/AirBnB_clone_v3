18f0d3eaf1bc33d9089d2716aa3f7421b633a073
bikaze <clmntmugisha@gmail.com>
2024-04-29 10:00:45 +0200

    Completed task 3

---
+++ api/v1/app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This is the Flask App to make the HBnB(AirBnB clone) alive"""
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+import os
+
+app = Flask(__name__)
+
+HOST = os.getenv('HBNB_API_HOST', '0.0.0.0')
+PORT = os.getenv('HBNB_API_PORT', 5000)
+
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardownDb(exception):
+    """This function closes the SQLAlchemy session"""
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host=HOST, port=PORT, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Returning the status of the api"""
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status', methods=['GET'])
+def status():
+    """handles the status route"""
+    return jsonify({"status": "OK"})
