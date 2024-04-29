4a1d2326427831c57adce565c786dccadf49200a
bikaze <clmntmugisha@gmail.com>
2024-04-29 10:09:46 +0200

    Fixed pep8 errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,10 +12,12 @@ PORT = os.getenv('HBNB_API_PORT', 5000)
 
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardownDb(exception):
     """This function closes the SQLAlchemy session"""
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host=HOST, port=PORT, threaded=True)
