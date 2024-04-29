b0643bbe713011ec97fbe00009a7fcc87ef1f3c2
ndiramiye2 <onlythenotes@gmail.com>
2024-04-29 22:50:03 +0200

    Nitafanya hivyo peke yangu

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -20,7 +20,6 @@ def get_state(state_id=None):
     return jsonify(state.to_dict())
 
 
-
 @app_views.route('states/<state_id>', methods=['DELETE'], strict_slashes=False)
 def delete_state(state_id=None):
     """A function to delete the state"""
